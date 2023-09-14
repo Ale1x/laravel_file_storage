806bbf46f75d0d8b97e4b7c00f166cf3969c0e2a
Alessandro Passarelli <alessandro@passarelli.dev>
2023-09-14 22:56:13 +0200

    Fix issue

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('login');
-});
+})->name('login');
 
 Route::get('/auth/github', [AuthController::class, 'redirectToProvider']);
 
