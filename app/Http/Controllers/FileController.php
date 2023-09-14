aaaf1b6e7207956d9e6087210e019dcc78608fde
Alessandro Passarelli <alessandro@passarelli.dev>
2023-09-14 20:46:14 +0200

    Added debug

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use GrahamCampbell\GitHub\Facades\GitHub;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 
 class FileController extends Controller
 {
@@ -20,29 +21,43 @@ class FileController extends Controller
             $extension = $file->getClientOriginalExtension();
             [$class, $subject, $chapter, $actualFileName] = explode('_', pathinfo($fileName, PATHINFO_FILENAME)); 
 
+            // Salva il file in una cartella temporanea
+            $tempPath = $file->storeAs('temp', $fileName);
+
             $folderPath = "$class/$subject/$chapter";
-            $actualFileNameWithExtension = "$actualFileName.$extension"; // aggiunta l'estensione
-            $content = base64_encode(file_get_contents($file)); // codifica in base64
+            $actualFileNameWithExtension = "$actualFileName.$extension"; 
+
+            // Legge il contenuto del file temporaneo
+            $content = base64_encode(Storage::get($tempPath));
 
             Log::info('Tentativo di caricare il file su GitHub');
             
-            $outputContent = base64_decode($content);   
-            
+            $outputContent = base64_decode($content);
+
+            // Configura il token GitHub per l'utente corrente
             config(['github.connections.main.token' => $user->github_token]);
 
             GitHub::repo()->contents()->create(
-                $user->commiter_name,  
-                $user->repository_name,  
+                $user->commiter_name,
+                $user->repository_name,
                 "$folderPath/$actualFileNameWithExtension",
                 $outputContent,
                 $user->commit_message
             );
+
+            // Rimuove il file temporaneo
+            Storage::delete($tempPath);
 
             Log::info('File caricato con successo');
 
             return redirect('/upload')->with('status', 'File Caricato con successo!');
         } catch (\Exception $e) {
             Log::error("Errore nel caricamento del file: {$e->getMessage()}");
+
+            Log::info('Token GitHub: ' . $user->github_token);
+            Log::info('Commiter Name: ' . $user->commiter_name);
+            Log::info('Repository Name: ' . $user->repository_name);
+
             return redirect('/upload')->with('status', 'Errore nel caricamento del file');
         }
     }
