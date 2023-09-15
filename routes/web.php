067a34e3b2dcd396aa8dbcea50f0e51fbb7430f7
Alessandro Passarelli <alessandro@passarelli.dev>
2023-09-15 18:06:22 +0200

    Removed unnecessary imports

--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,34 @@
-<?php
+    <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\FileController;
-use App\Http\Controllers\UserProfileController;
-use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\AuthController;
+    use App\Http\Controllers\FileController;
+    use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+    /*
+    |--------------------------------------------------------------------------
+    | Web Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register web routes for your application. These
+    | routes are loaded by the RouteServiceProvider and all of them will
+    | be assigned to the "web" middleware group. Make something great!
+    |
+    */
 
 
-Route::get('/', function () {
-    return view('login');
-})->name('login');
+    Route::get('/', function () {
+        return view('login');
+    })->name('login');
 
-Route::get('/auth/github', [AuthController::class, 'redirectToProvider']);
+    Route::get('/auth/github', [AuthController::class, 'redirectToProvider']);
 
-Route::get('/auth/callback', [AuthController::class, 'handleProviderCallback']);
+    Route::get('/auth/callback', [AuthController::class, 'handleProviderCallback']);
 
-Route::get('/upload', function() {
-    return view('upload');
-})->middleware('auth');
+    Route::get('/upload', function() {
+        return view('upload');
+    })->middleware('auth');
 
-Route::post('/upload', [FileController::class, 'handleUpload'])->middleware('auth');
+    Route::post('/upload', [FileController::class, 'handleUpload'])->middleware('auth');
 
-Route::get('/profilo', [AuthController::class, 'index'])->middleware('auth');
-Route::post('/profilo', [AuthController::class, 'update'])->name('profilo.update')->middleware('auth');
+    Route::get('/profilo', [AuthController::class, 'index'])->middleware('auth');
+    Route::post('/profilo', [AuthController::class, 'update'])->name('profilo.update')->middleware('auth');
